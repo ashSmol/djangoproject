9e0740fb06e2e9477909378a574bb5f753102e96
AshSmol <ashiryaev84@gmail.com>
2021-07-26 13:56:18 +0300

    added user_profiles

--- geekshop/authapp/views.py
+++ geekshop/authapp/views.py
@@ -1,8 +1,9 @@
 from django.core.mail import send_mail
+from django.db import transaction
 from django.shortcuts import render, HttpResponseRedirect
 
 from geekshop import settings
-from .forms import ShopUserLoginForm, ShopUserRegisterForm, ShopUserEditForm
+from .forms import ShopUserLoginForm, ShopUserRegisterForm, ShopUserEditForm, ShopUserProfileEditForm
 from django.contrib import auth
 from django.urls import reverse
 
@@ -92,20 +93,29 @@ def verify(request, email, activation_key):
         return HttpResponseRedirect(reverse('index'))
 
 
+@transaction.atomic
 def edit(request):
     title = 'редактирование'
+    user = request.user
 
     if request.method == 'POST':
-        edit_form = ShopUserEditForm(request.POST, request.FILES, instance=request.user)
-        if edit_form.is_valid():
+        edit_form = ShopUserEditForm(request.POST, request.FILES, instance=user)
+        profile_form = ShopUserProfileEditForm(request.POST, instance=user.shopuserprofile)
+        if edit_form.is_valid() and profile_form.is_valid():
             edit_form.save()
             return HttpResponseRedirect(reverse('auth:edit'))
     else:
-        edit_form = ShopUserEditForm(instance=request.user)
-
-    context = {'title': title, 'edit_form': edit_form}
-
-    return render(request, 'authapp/edit.html', context)
+        edit_form = ShopUserEditForm(instance=user)
+        profile_form = ShopUserProfileEditForm(
+            instance=request.user.shopuserprofile
+        )
+    content = {
+        'title': title,
+        'edit_form': edit_form,
+        'profile_form': profile_form,
+        'user': user,
+    }
+    return render(request, 'authapp/edit.html', content)
 
 
 def send_verify_mail(user):
